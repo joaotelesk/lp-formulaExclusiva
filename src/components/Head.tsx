64cdd5f83498a295feb39fbe3b84846456cd03ba
Joao Teles <joaojoseteles@gmail.com>
2023-04-11 00:06:31 -0300

    :fire: removendo arquivos

--- src/components/Head.tsx
+++
@@ -1,9 +0,0 @@
-export function Head() {
-  return (
-    <>
-      <h1 className="text-lg font-normal">
-        <span className="font-bold text-yellow">Formula</span> Exclusiva
-      </h1>
-    </>
-  );
-}
