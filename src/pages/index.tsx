066c5ca7c28649b09d4419aa51c70c32d5e21f45
Joao Teles <joaojoseteles@gmail.com>
2023-04-20 18:03:19 -0300

    :recycle: refactor: utilizando Section5 e Section6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { Section1 } from "@/components/Home/Section1";
 import { Section2 } from "@/components/Home/Section2";
 import { Section3 } from "@/components/Home/Section3";
 import { Section4 } from "@/components/Home/Section4";
+import { Section5 } from "@/components/Home/Section5";
+import { Section6 } from "@/components/Home/Section6";
 
 // Utilities
 // Interfaces
@@ -16,6 +18,8 @@ export default function Home() {
         <Section2 />
         <Section3 />
         <Section4 />
+        <Section5 />
+        <Section6 />
       </main>
     </>
   );
